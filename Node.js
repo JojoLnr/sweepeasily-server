13a56ce31b53646fa9b94cd215eb8f21d653cd8b
Jojolnr <mbarga.joel.christian@gmail.com>
2025-04-24 21:26:05 -0500

    Add Firebase admin setup with environment variables

---
+++ Node.js
@@ -0,0 +1,12 @@
+require('dotenv').config(); // Make sure this is at the top of your file
+
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.FIREBASE_DB_URL,
+});
